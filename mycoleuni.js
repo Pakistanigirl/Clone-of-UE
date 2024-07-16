f652f42657b41abb7a185fa84a2f5edd165e5b32
Madiha Fatima <144526926+Pakistanigirl@users.noreply.github.com>
2024-07-16 08:25:08 -0700

    Add files via upload

---
+++ mycoleuni.js
@@ -0,0 +1,8 @@
+"use strict";
+// alert(location.href);
+// if(confirm("Wanna go to original web page of university of Education???")){
+//     location.href="https://ue.edu.pk/campus.php?id=42"; // redirect the browser to another URL
+//     }
+
+
+
